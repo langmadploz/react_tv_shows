b555f08719a8d6c66c5ad9a6cbfd0b8e8315991b
Lang Ploszek <langmfiser@gmail.com>
2022-08-23 09:55:15 -0600

    props passed through except genre

---
+++ src/components/Show.js
@@ -0,0 +1,14 @@
+import Genre from './Genre'
+
+const Show = {props} => {
+  return (
+    <div className="show">
+      <h1>{props.show.title}</h1>
+      <img src={props.show.image_path} />
+      <p>{props.show.description}</p>
+      <h3>Years: {props.show.years}</h3>
+      <h3>Network: {props.show.network}</h3>
+      <h3>Rotten Tomatoes Score: {props.show.rotten_tomatoes}</h3>
+    </div>
+  )
+}
